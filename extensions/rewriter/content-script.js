f365d6f3b434b1a8111189aa8bb7feed81ee4d7a
anirudhb <anirudhb@users.noreply.github.com>
2024-09-20 22:35:33 -0700

    tweaks for submitting to AMO

--- extensions/rewriter/content-script.js
+++ extensions/rewriter/content-script.js
@@ -59,59 +59,60 @@ async function init(text_) {
 	dialog.init();
 }
 
-const dialogTemplate = document.createElement("template");
-dialogTemplate.innerHTML = `
-<style>
-.container {
-	max-width: min(90vw, 90%);
-	display: flex;
-	flex-direction: row;
-	gap: 8px;
-	background: white;
-	padding: 16px;
-	border: 1px solid grey;
-	user-select: none;
-	max-height: 90px;
-}
-#response {
-	flex-grow: 1;
-	user-select: all;
-	overflow-y: auto;
-}
-.btn-group, .btn-group-h {
-	display: flex;
-	flex-direction: column;
-	gap: 3px;
-}
-.btn-group-h {
-	flex-direction: row;
-}
-.icon {
-  width: 1em;
-  height: 1em;
-  vertical-align: -0.125em;
-}
-</style>
-<div class="container">
-	<!-- <button id="prev" disabled>Previous</button> -->
-	<div id="response"></div>
-	<div class="btn-group">
-		<button id="accept" disabled>Accept</button>
-		<!-- <button id="next" disabled>Next</button> -->
-		<div class="btn-group-h">
-			<!-- FontAwesome chevron-left -->
-			<button id="prev" disabled>
-				<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>
-			</button>
-			<!-- FontAwesome chevron-right -->
-			<button id="next" disabled>
-				<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>
-			</button>
+const dialogTemplate = (new DOMParser()).parseFromString(`
+<template>
+	<style>
+	.container {
+		max-width: min(90vw, 90%);
+		display: flex;
+		flex-direction: row;
+		gap: 8px;
+		background: white;
+		padding: 16px;
+		border: 1px solid grey;
+		user-select: none;
+		max-height: 90px;
+	}
+	#response {
+		flex-grow: 1;
+		user-select: all;
+		overflow-y: auto;
+	}
+	.btn-group, .btn-group-h {
+		display: flex;
+		flex-direction: column;
+		gap: 3px;
+	}
+	.btn-group-h {
+		flex-direction: row;
+	}
+	.icon {
+		width: 1em;
+		height: 1em;
+		vertical-align: -0.125em;
+	}
+	</style>
+	<div class="container">
+		<!-- <button id="prev" disabled>Previous</button> -->
+		<div id="response"></div>
+		<div class="btn-group">
+			<button id="accept" disabled>Accept</button>
+			<!-- <button id="next" disabled>Next</button> -->
+			<div class="btn-group-h">
+				<!-- FontAwesome chevron-left -->
+				<button id="prev" disabled>
+					<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>
+				</button>
+				<!-- FontAwesome chevron-right -->
+				<button id="next" disabled>
+					<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>
+				</button>
+			</div>
+			<span id="responseNum"></span>
 		</div>
-		<span id="responseNum"></span>
 	</div>
-</div>
-`.trim();
+</template>
+`.trim(), "text/html").querySelector("template");
 
 class TheDialog {
 	#holder;
